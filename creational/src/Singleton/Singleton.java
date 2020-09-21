8e072e319d33dda6d7338e6e9242c455a27280dc
siddhantdeshmukh99 <siddhantdeshmukh99@gmail.com>
2020-09-21 22:55:40 +0530

    small file changes

--- creational/src/Singleton/Singleton.java
+++ creational/src/Singleton/Singleton.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Problem1;
+package Singleton;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
